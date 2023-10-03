4ce465a93b56f7ffac1fe41f64a73ba254a553a7
Patrick Piotrowski <patrick.santos@vidyatec.com>
2023-10-03 10:41:52 -0300

    reached lesson 26

--- project_19/src/script.js
+++ project_19/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 THREE.ColorManagement.enabled = false
 
@@ -15,6 +16,15 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.3)
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.7)
+directionalLight.position.set(1, 2, 3)
+scene.add(ambientLight, directionalLight)
+
 
 /**
  * Objects
@@ -37,6 +47,25 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+// Raycaster
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// object1.updateMatrixWorld()
+// object2.updateMatrixWorld()
+// object3.updateMatrixWorld()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect)
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
+
 
 /**
  * Sizes
@@ -62,6 +91,32 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = -(event.clientY / sizes.height * 2 - 1)
+})
+
+window.addEventListener('click', () => {
+    if(currentIntersect) {
+        switch(currentIntersect.object) {
+            case object1:
+                console.log('click on object 1')
+                break
+            case object2:
+                console.log('click on object 2')
+                break
+            case object3:
+                console.log('click on object 3')
+                break
+        }
+    }
+})
+
+
+/**
  * Camera
  */
 // Base camera
@@ -84,13 +139,80 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+let model = null
+
+gltfLoader.load(
+    '/models/Duck/glTF-Binary/Duck.glb',
+    (gltf) => {
+        model = gltf.scene
+        model.position.y = -1.2
+        scene.add(model)
+    }
+    )
+
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
 
+let currentIntersect = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3)
+    object2.position.y = Math.cos(elapsedTime)
+    object3.position.y = Math.sin(elapsedTime)
+
+    // Cast ray
+    raycaster.setFromCamera(mouse, camera)
+
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
+
+    // raycaster.set(rayOrigin, rayDirection)
+
+    const objsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objsToTest)
+    
+    for(const obj of objsToTest) {
+        obj.material.color.set('#ff0000')
+    }
+
+    for(const intersect of intersects) {
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    if(intersects.length) {
+        if(!currentIntersect) {
+            console.log('mouse enter')
+        }
+        currentIntersect = intersects[0]
+    }
+    else {
+        if(currentIntersect) {
+            console.log('mouse leave')
+        }
+        currentIntersect = null
+    }
+
+    // intersect with model
+    if(model) {
+        const modelIntersect = raycaster.intersectObject(model)
+        if(modelIntersect.length) {
+            model.scale.set(1.5, 1.5, 1.5)
+        }
+        else{
+            model.scale.set(1, 1, 1)
+        }
+    }
 
     // Update controls
     controls.update()
