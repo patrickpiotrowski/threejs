725d7040fe79b9ba630e0d84d3938c1eac315f3e
Patrick Piotrowski <patrick.santos@vidyatec.com>
2023-10-18 14:00:55 -0300

    lesson 33

--- project_28_post_processing/src/script.js
+++ project_28_post_processing/src/script.js
@@ -18,9 +18,9 @@ const scene = new THREE.Scene()
 /**
  * Loaders
  */
-const textureLoader = new THREE.TextureLoader()
 const gltfLoader = new GLTFLoader()
 const cubeTextureLoader = new THREE.CubeTextureLoader()
+const textureLoader = new THREE.TextureLoader()
 
 /**
  * Update all materials
@@ -31,7 +31,7 @@ const updateAllMaterials = () =>
     {
         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
         {
-            child.material.envMapIntensity = 1
+            child.material.envMapIntensity = 2.5
             child.material.needsUpdate = true
             child.castShadow = true
             child.receiveShadow = true
@@ -55,118 +55,16 @@ scene.background = environmentMap
 scene.environment = environmentMap
 
 /**
- * Material
- */
-
-// Textures
-const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg')
-mapTexture.colorSpace = THREE.SRGBColorSpace
-const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
-
-// Material
-const material = new THREE.MeshStandardMaterial( {
-    map: mapTexture,
-    normalMap: normalTexture
-})
-
-const depthMaterial = new THREE.MeshDepthMaterial({
-    depthPacking: THREE.RGBADepthPacking
-})
-
-const customUniforms = {
-    uTime: { value: 0 }
-}
-
-material.onBeforeCompile = (shader) => {
-
-    shader.uniforms.uTime = customUniforms.uTime
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <common>',
-        `
-            #include <common>
-
-            uniform float uTime;
-
-            mat2 get2dRotateMatrix(float _angle)
-            {
-                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
-            }
-        `
-    )
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <beginnormal_vertex>',
-        `
-            #include <beginnormal_vertex>
-
-            float angle = (position.y + uTime) * 0.9;
-
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
-
-            objectNormal.xz = rotateMatrix * objectNormal.xz;
-
-        `
-    )
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <begin_vertex>', 
-        `
-            #include <begin_vertex>
-            
-            transformed.xz = rotateMatrix * transformed.xz;
-
-        `
-        )
-}
-
-depthMaterial.onBeforeCompile = (shader) => {
-    shader.uniforms.uTime = customUniforms.uTime
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <common>',
-        `
-            #include <common>
-
-            uniform float uTime;
-
-            mat2 get2dRotateMatrix(float _angle)
-            {
-                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
-            }
-        `
-    )
-
-    shader.vertexShader = shader.vertexShader.replace(
-        '#include <begin_vertex>', 
-        `
-            #include <begin_vertex>
-            
-            float angle = (position.y + uTime) * 0.9;
-
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
-
-            transformed.xz = rotateMatrix * transformed.xz;
-
-        `
-        )
-}
-
-/**
  * Models
  */
 gltfLoader.load(
-    '/models/LeePerrySmith/LeePerrySmith.glb',
+    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
     (gltf) =>
     {
-        // Model
-        const mesh = gltf.scene.children[0]
-        mesh.rotation.y = Math.PI * 0.5
-        mesh.material = material
-        mesh.customDepthMaterial = depthMaterial
-        scene.add(mesh)
+        gltf.scene.scale.set(2, 2, 2)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
 
-        // Update materials
         updateAllMaterials()
     }
 )
@@ -179,7 +77,7 @@ directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 2, - 2.25)
+directionalLight.position.set(0.25, 3, - 2.25)
 scene.add(directionalLight)
 
 /**
@@ -227,8 +125,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFShadowMap
 renderer.useLegacyLights = false
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 1
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 1.5
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -240,9 +138,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    //Update material
-    customUniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
